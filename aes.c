43d6b1969b3c7f9562de202a4b40c1aa43ecd279
Ryuzaou325 <talhaahmad@gmx.com>
2025-03-25 15:05:52 +0100

    created aes

---
+++ aes.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <sodium.h>
+
+int main() {
+  #define MESSAGE (const unsigned char *) "test"
+  #define MESSAGE_LEN 4
+  #define ADDITIONAL_DATA (const unsigned char *) "123456"
+  #define ADDITIONAL_DATA_LEN 6
+
+  unsigned char nonce[crypto_aead_aegis256_NPUBBYTES];
+  unsigned char key[crypto_aead_aegis256_KEYBYTES];
+  unsigned char ciphertext[MESSAGE_LEN + crypto_aead_aegis256_ABYTES];
+  unsigned long long ciphertext_len;
+
+  crypto_aead_aegis256_keygen(key);
+  randombytes_buf(nonce, sizeof nonce);
+
+  crypto_aead_aegis256_encrypt(ciphertext, &ciphertext_len,
+    MESSAGE, MESSAGE_LEN,
+    ADDITIONAL_DATA, ADDITIONAL_DATA_LEN,
+    NULL, nonce, key);
+
+  unsigned char decrypted[MESSAGE_LEN];
+  unsigned long long decrypted_len;
+  if (crypto_aead_aegis256_decrypt(
+    decrypted, &decrypted_len,
+    NULL,
+    ciphertext, ciphertext_len,
+    ADDITIONAL_DATA,
+    ADDITIONAL_DATA_LEN,
+    nonce, key) 
+  == 0) {
+    printf("Success\n");
+  }
+  else {
+    printf("Failure\n");
+  }
+}
